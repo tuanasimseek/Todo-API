292959a3bbc170f384f70ebb0a02097b426d9cc5
tuanasimseek <tuanas5@icloud.com>
2026-04-30 10:22:40 +0300

    tasks

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Task
+from .serializers import TaskSerializer
 
-# Create your views here.
+class TaskViewSet(viewsets.ModelViewSet):
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        queryset = Task.objects.filter(user=self.request.user)
+
+        completed = self.request.query_params.get("completed")
+        if completed is not None:
+            queryset = queryset.filter(completed=completed.lower() == "true")
+
+        return queryset
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
